44e6e542bf9a7f3a4e28131e6a3732ecea71197f
Cristian Franco <cristianmusic7@gmail.com>
2017-12-16 06:10:15 -0500

    React​ ​JS​ ​Grid​ ​Enhancements
    ComboBoxEditor, CheckBoxEditor and CustomEditor Implementation.
    Data Generation logic separated from Rendering logic.

--- my-demo/src/components/HeaderRenderer.js
+++ my-demo/src/components/HeaderRenderer.js
@@ -1,6 +1,6 @@
 import React, { Component, onInit } from 'react'
 import ReactPaginate from 'react-paginate';
-import { HeaderRow } from '@sans/react-data-grid/packages/react-data-grid/dist/react-data-grid';
+import { HeaderRow } from '@lunarkid/react-data-grid/packages/react-data-grid/dist/react-data-grid';
 
 export default class HeaderRenderer extends Component{
   constructor(props) {
@@ -10,7 +10,7 @@ export default class HeaderRenderer extends Component{
 
 
   render() {
-    return ( <div className={this.props.columnClass} > 
+    return ( <div className={this.props.columnClass} >
     <HeaderRow ref = "row" { ...this.props } />
       </div> );
   }
